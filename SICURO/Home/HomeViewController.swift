aef2217fde129bfe0e7088b6dfba5c9e5b283b92
Shashank Shukla <shashankshukla@Shashanks-MacBook-Pro.local>
2023-10-24 22:40:12 +0530

    added search table view

--- SICURO/Home/HomeViewController.swift
+++ SICURO/Home/HomeViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 import MapKit
 
-class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+struct Location {
+    let title: String
+    let coordinate: CLLocationCoordinate2D?
+}
+
+class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var startLocationTextField: UITextField!
     @IBOutlet weak var endLocationTextField: UITextField!
@@ -16,17 +21,63 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapView
     @IBOutlet weak var bookCabButton: UIButton!
     
     let locationManager = CLLocationManager()
+    var locations = [Location]()
+    let searchTableView :UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 //    let searchVC = UISearchController(searchResultsController: ResultsViewController())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
+        view.addSubview(searchTableView)
         mapView.delegate = self
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        searchTableView.isHidden = true
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let tableY = startLocationTextField.frame.origin.y+startLocationTextField.frame.height+5
+        searchTableView.frame = CGRect(x: 0, y: tableY, width: view.frame.size.width, height: view.frame.size.height-tableY)
+        
+        startLocationTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        startLocationTextField.resignFirstResponder()
+        if let text = startLocationTextField.text, !text.isEmpty {
+            self.getAddress(address: text) { [weak self] locations in
+                DispatchQueue.main.async {
+                    self?.locations = locations
+                    self?.searchTableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = searchTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = locations[indexPath.row].title
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //notify
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -47,14 +98,32 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapView
         mapView.addAnnotation(pin)
     }
     
-    func getAddress(address: String) {
+    func getAddress(address: String, completion: @escaping (([Location]) -> Void)) {
         let geoCoder = CLGeocoder()
         geoCoder.geocodeAddressString(address) { placemarks, error in
-            guard let placemarks = placemarks, let location = placemarks.first?.location else {
+            guard let placemarks = placemarks, error == nil else {
                 print("no location found")
+                completion([])
                 return
             }
-            
+            let models: [Location] = placemarks.compactMap({ places in
+                var name = ""
+                if let locationName = places.name {
+                    name += locationName
+                }
+                if let region = places.administrativeArea {
+                    name += ", \(region)"
+                }
+                if let locality = places.locality {
+                    name += ", \(locality)"
+                }
+                if let country = places.country {
+                    name += ", \(country)"
+                }
+                let result = Location(title: "", coordinate: places.location?.coordinate)
+                return result
+            })
+            completion(models)
         }
     }
     
